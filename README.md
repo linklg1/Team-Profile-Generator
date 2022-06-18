330450edab4a6f131019282731e53c22776c750c
Zelda <lumakawar@hotmail.com>
2022-06-18 17:48:42 -0400

    success

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Team-Profile-Generator
+# Team Profile Generator
+
+This is an app that takes in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person.
+
+## Table of Contents 
+
+ 1. [Installation](#Installation)
+ 2. [Usage](#Usage)
+ 3. [Contributing](#Contributing)
+ 4. [Liscense](#Liscense)
+ 5. [VideoLink](#VideoLink)
+ 6. [ScreenShots](#ScreenShots)
+ 7. [Repo](#Repo)
+ 8. [GeneratedHTML](#GeneratedHTML)
+
+
+## Installation
+
+Packages that need installation include: Inquirer
+
+
+```bash
+npm install inquirer
+```
+
+## Usage
+
+```bash
+? What is your name? Link
+? What is your employee id number? 123
+? What is your email? link@hotmail.com
+? What is your office number? 456
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Liscense
+None
+
+## VideoLink 
+Video of running app:
+
+## ScreenShots
+
+
+## Repo
+
+https://github.com/linklg1/Team-Profile-Generator
+
+## GeneratedHTML
+
+
--- app.js
+++ app.js
@@ -36,24 +36,94 @@ const promptManager = () => {
           }   
     ])
     .then(response => {
-        const ManagerInstance = new Manager (response.name, response.id, response.email, response.officeNumber)
+        const ManagerInstance = new Manager (response.name, parseInt(response.id), response.email, parseInt(response.officeNumber))
         teamobjectarray.push(ManagerInstance)
-    // call option function here (create option function elsewhere fire init when they say no)
-    init();
+    // call option function here
+    addTeamMember();
     })
 }
-// need another couple functions with inquirer 
-//prompt that asks if you want to add an engineer or intern for the .then it 
-//will run init if they select no and it will run following prompts if they select yes
-//typ list name addteammate message do you want to add a team mate > options would be add engineer add intern or no thank you
-// .then(response => { if (response.addteammate === add engineer) {promptengineer ();}})
 
+const promptEngineer = () => {
+  inquirer.prompt ([
+      {
+          type: 'input',
+          name: 'name',
+          message: 'What is your name?',
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'What is your employee id number?',
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'What is your email?',
+        },
+        {
+          type: 'input',
+          name: 'GitHub',
+          message: 'What is your GitHub Username?',
+        }   
+  ])
+  .then(response => {
+      const EngineerInstance = new Engineer (response.name, parseInt(response.id), response.email, response.GitHub)
+      teamobjectarray.push(EngineerInstance)
+      addTeamMember();
+  })}
 
+  const promptIntern = () => {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+          },
+          {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee id number?',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+          },
+          {
+            type: 'input',
+            name: 'SchoolName',
+            message: 'What is the name of your school?',
+          }   
+    ])
+    .then(response => {
+        const InternInstance = new Intern (response.name, parseInt(response.id), response.email, response.SchoolName)
+        teamobjectarray.push(InternInstance)
+        addTeamMember();
+    })}
+  
 
+const addTeamMember = () => {
+  inquirer.prompt ([
+    {
+      type: 'list',
+      name: 'addteammember',
+      message: 'Would you like to add an additional team member?',
+      choices: ["Engineer", "Intern", "No thank you"]
+    },
+  ]) 
+  .then (response => {
+    if (response.addteammember === "Engineer") {
+      promptEngineer();
+    }
+    else if (response.addteammember === "Intern") {
+      promptIntern();
+    }
+    else {
+      init();
+    }
+  })
 
-
-
-
+}
 
 const init = () => {
     
